d796be5606894f04fa31feb7a53a6e46364a533c
Gabriel Savard <2038783@csfoy.ca>
2025-02-20 22:11:34 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import "bootstrap"
+    import "bootstrap/dist/css/bootstrap.min.css"
+
+    import Timeline from './components/Timeline.vue'
+</script>
+
+<template>
+    <div class="container">
+        <Timeline />
+    </div>
+</template>
+
+<style scoped>
+</style>
